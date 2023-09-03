f731acc97d1a021789cd511b05cd8f5a29ea1405
cody koeninger <cody@koeninger.org>
2023-09-03 18:37:30 -0500

    csv and binary writers

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,84 @@
 use std::cell::{ RefCell, Cell };
+use std::io::{ Result, Write };
 
 #[cfg(all(not(feature = "off"), feature = "capacity_1_million"))]
-const CAPACITY: usize = 1_000_000 * 3;
+pub const CAPACITY: usize = 1_000_000 * 3;
 
 #[cfg(all(not(feature = "off"), feature = "capacity_8_million"))]
-const CAPACITY: usize = 8_000_000 * 3;
+pub const CAPACITY: usize = 8_000_000 * 3;
 
 #[cfg(all(not(feature = "off"), feature = "capacity_16_million"))]
-const CAPACITY: usize = 16_000_000 * 3;
+pub const CAPACITY: usize = 16_000_000 * 3;
 
 #[cfg(all(not(feature = "off"), feature = "capacity_32_million"))]
-const CAPACITY: usize = 32_000_000 * 3;
+pub const CAPACITY: usize = 32_000_000 * 3;
 
 #[cfg(all(not(feature = "off"), feature = "capacity_64_million"))]
-const CAPACITY: usize = 64_000_000 * 3;
+pub const CAPACITY: usize = 64_000_000 * 3;
 
 #[cfg(feature = "off")]
-const CAPACITY: usize = 0;
+pub const CAPACITY: usize = 0;
 
 #[cfg(all(not(feature = "off"), not(feature = "capacity_1_million"), not(feature = "capacity_8_million"), not(feature = "capacity_16_million"), not(feature = "capacity_32_million"), not(feature = "capacity_64_million")))]
 compile_error!("tsc-trace requires enabling exactly one of the features 'capacity_1_million' ... 'capacity_64_million', or 'off'");
 #[cfg(all(not(feature = "off"), not(feature = "capacity_1_million"), not(feature = "capacity_8_million"), not(feature = "capacity_16_million"), not(feature = "capacity_32_million"), not(feature = "capacity_64_million")))]
-const CAPACITY: usize = 0;
+pub const CAPACITY: usize = 0;
 
 thread_local! {
     static TSC_TRACE_SPANS: RefCell<[u64; CAPACITY]> = const { RefCell::new([0; CAPACITY]) };
     static TSC_TRACE_INDEX: Cell<usize> = const { Cell::new(0) };
 }
 
-pub struct Printer {}
-
-// TODO replace with macro
-pub fn print_traces() {
+/// Writes the current thread's array of traces in the format
+///
+/// tag,start_rdtsc,stop_rdtsc,stop_minus_start\n
+///
+/// Stops writing once it encounters a stop_rdtsc of zero,
+/// assuming that's an unused portion of the array
+pub fn write_csv(writer: &mut impl Write) -> Result<()> {
+    let mut res = Ok(());
     TSC_TRACE_SPANS.with(|spans| {
         let spans = spans.borrow();
         for i in (0..CAPACITY).step_by(3) {
             let tag = spans[i];
             let start = spans[i+1];
             let stop = spans[i+2];
-            println!("{tag} {start} {stop} {}", stop - start);
+            if stop == 0 {
+                break;
+            }
+            if let e @ Err(_) = writeln!(writer, "{tag},{start},{stop},{}", stop - start) {
+                res = e;
+                break;
+            }
         }
     });
+    res
 }
 
-impl Drop for Printer {
-    fn drop(&mut self) {
-        print_traces();
-    }
+/// Writes the current thread's array of traces in a binary format.
+/// This is, in order
+///
+/// tag: u64
+/// start_rdtsc: u64
+/// stop_rdtsc: u64
+///
+/// There are no delimiters between each field or between traces.
+/// Assumes little-endian since this library only works for x86.
+/// Unlike print_csv, the difference between stop and start is not calculated.
+/// Writes the entire array, even zeroed / unused portions.
+///
+/// This is suitable for import to Clickhouse via format RowBinary
+/// <https://clickhouse.com/docs/en/interfaces/formats#rowbinary>
+pub fn write_binary(writer: &mut impl Write) -> Result<()> {
+    let mut res = Ok(());
+    TSC_TRACE_SPANS.with(|spans| {
+        let spans = spans.borrow();
+        let bytes: &[u8] = bytemuck::cast_slice(&*spans);
+        if let e @ Err(_) = writer.write_all(&bytes) {
+            res = e;
+        }
+    });
+    res
 }
 
 #[inline(always)]
@@ -69,6 +101,7 @@ pub fn rdtsc() -> u64 {
 #[cfg(target_arch = "x86_64")]
 pub fn rdtsc() -> u64 {
     use core::arch::x86_64::_rdtsc;
+    #[cfg(feature = "lfence")]
     use core::arch::x86_64::_mm_lfence;
     unsafe {
         #[cfg(feature = "lfence")]
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use tsc_trace::*;
 
+// anything that `as u64` works on can be used as a tag for traces
 enum Traces {
     Main = 0,
     SomeFunction = 1,
 }
 
-fn main() {
-    let _p = Printer {};
-    trace!(Traces::Main);
-    for _ in 1..10 {
-        some_function();
-    }
+fn main() -> std::io::Result<()> {
+    {
+        trace!(Traces::Main); // reads rdtsc to get cycle count, stores it in a stack variable
+        for _ in 1..10 {
+            some_function();
+        }
+    } // drop impl of that variable reads rdtsc again, stores the tag and both cycle counts in a thread local array
+
+    // write the array of traces to binary file
+    let mut bin = std::fs::File::create("/tmp/traces")?;
+    write_binary(&mut bin)?;
+
+    // write the array of traces to stdout in comma-separated format
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
+    write_csv(&mut lock)?;
+
+    Ok(())
 }
 
 #[inline]
